af86c2c5e5d5972a63082b903a54fef750f2d37b
VentGrey <omarpurataf@gmail.com>
2021-01-02 22:20:07 -0600

    Finished install script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/bash
+
+USR_DIR="/usr/share/icons/"
+
+# Check who is running this script
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "This will install Marwaita in $HOME/.icons/"
+    echo "Do you want to proceed? Y/n:"
+    read  -r ans
+
+    case $ans in
+        [Yy]* )
+            rm -rf ~/.icons/Marwaita*;
+            mv Marwaita* ~/.icons;
+            echo -e "[\e[32mOK\e[39m] Icons installed in \e[33m$HOME/.icons/\e[39m";
+            exit 0
+            ;;
+
+        [Nn]* )
+            echo "Installation cancelled";
+            exit 0
+            ;;
+
+        *)
+            echo "Invalid answer!";
+            exit 1
+            ;;
+    esac
+else
+    echo "This will install Marwaita in /usr/share/icons"
+    read -pr "Do you want to proceed? Y/n" ans
+
+    case $ans in
+        y|Y )
+            rm -rf /usr/share/icons/Marwaita*;
+            mv Marwaita* $USR_DIR;
+            echo -e "[\e[32mOK\e[39m] Icons installed \e[33m$USR_DIR\e[39m";
+            exit 0
+            ;;
+
+        n|N )
+            echo "Installation cancelled";
+            exit 0;;
+
+        *)
+            echo "Invalid answer!";
+            exit 1;;
+    esac
+fi
